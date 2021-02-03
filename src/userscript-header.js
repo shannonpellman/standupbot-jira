e008388d5c2f7a6b2b93d575504593dfd82a3ee1
Zach Pellman <zpellman@gmail.com>
2021-02-02 20:06:16 -0700

    Initial commit

---
+++ src/userscript-header.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        StandupBot
+// @namespace   https://github.com/zpellman
+// @version     0.0.3
+// @description Tampermonkey user script to help run standup from a Jira Scrum board
+// @author      Zach Pellman <zpellman@gmail.com>
+// @match       https://*/jira/*
+// @grant       none
+// @require     https://fb.me/react-0.14.3.js
+// @updateUrl   https://raw.githubusercontent.com/zpellman/standupbot-jira/main/dist/standupbot-jira.jsgit
+// ==/UserScript==
